9e327d69cb18f7210c12d7df331f042295a18140
kagudkov <kagudkov@gmail.com>
2017-04-02 21:23:08 +0300

    граф наследования

--- TypeInfo.h
+++ TypeInfo.h
@@ -30,10 +30,34 @@ struct TypeInfo{
 static std::map<std::string, TypeInfo> TypeInfoForClasses;
 static std::map<std::string, std::vector<std::string> > graph;
 
+struct registrar{
+    registrar(std::string derived, std::string base){
+        if(graph.find(base) == graph.end()){
+            graph[base] = std::vector<std::string>();
+        }
+        graph[base].push_back(derived);
+        for(auto i = graph.begin(); i != graph.end(); ++i){
+            for( int j = 0; j < i->second.size(); ++j){
+                if(i->second[j] == base){
+                    graph[i->first].push_back(derived);
+                }
+            }
+        }
+    }
+};
+
 #define NEW(T, o) new T();                             \
-TypeInfoForClasses[std::string(#o)] = TypeInfo(#T); \
+TypeInfoForClasses[std::string(#o)] = TypeInfo(#T);
 
 
 #define TYPEID(o) TypeInfoForClasses[std::string(#o)]
+
+#define EXTENDS(D, B) D : B { \
+registrar r = registrar(#D, #B);
+
+#define MEXTENDS(D, B1, B2) D : B1, B2 { \
+registrar r1 = registrar(#D, #B1); \
+registrar r2 = registrar(#D, #B2);
+
 
 #endif //RTTI_TYPEINFO_H
--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include "TypeInfo.h"
-
+#include <typeinfo>
 struct Base
 {
+    virtual void f(){}
     int k;
 };
 
-struct DerivedA : Base
-{
-    char c;
+struct EXTENDS(DerivedA, Base)
+    virtual void f(){}
+    int c = 10;
 };
 
-struct DerivedB : Base
-{
+struct EXTENDS(DerivedB, Base)
     int a;
     int b;
 };
 
 
-struct Derived : DerivedA, DerivedB
-{
+struct MEXTENDS(Derived, DerivedA, DerivedB)
     int a;
     int b;
 };
@@ -27,10 +26,18 @@ struct Derived : DerivedA, DerivedB
 int main() {
     Base *a = NEW(DerivedA, a);
     Base *b = NEW(Base, b);
+    DerivedA *d = NEW(Derived, d);
+    for( int i = -100; i < 100; ++i) {
+        if (reinterpret_cast<Derived *>(d + i)->c == 10) {
+            std::cout << i << " " << reinterpret_cast<Derived *>(d + i)->c << "\n";
+        }
+    }
+    std::cout << reinterpret_cast<Derived *>(d)->c << "\n";
+    //std::cout << dynamic_cast<Derived*>(d)->c << "\n";
     std::cout << TYPEID(a).name << "\n";
     std::cout << TYPEID(b).name << "\n";
     std::cout << "Base size = " << sizeof(Base) << " derivedA size = " << sizeof(DerivedA) << "\n";
     std::cout << "DerivedB size = " << sizeof(DerivedB) << " derived size = " << sizeof(Derived) << "\n";
-
+    std::cout << reinterpret_cast<DerivedA*>(a)->c;
     return 0;
 }
